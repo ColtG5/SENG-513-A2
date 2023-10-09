0b6955ca13ec249c6e956ba4a957f3dce30877aa
ColtG5 <colton.gowans@ucalgary.ca>
2023-10-08 20:23:53 -0600

    solid game grid

--- drawGameGrid.js
+++ drawGameGrid.js
@@ -1,25 +1,37 @@
-const gameGridSize = 20;
+const gameGridSize = 30;
+const gridItemSize = 10;
 
 document.addEventListener('DOMContentLoaded', function() {
     const gameGrid = document.getElementById('game-grid');
 
-    // const gridItem = document.createElement('div');
-    // gridItem.classList.add('grid-item');
+    gameGrid.style.setProperty('--grid-item-amount', gameGridSize);
+    gameGrid.style.setProperty('--grid-item-size', gridItemSize + 'px');
 
-    console.log(gameGrid.offsetWidth);
-    console.log(gameGrid.offsetHeight);
-
-    const howManyGridItemsWide = Math.floor(gameGrid.offsetWidth / gameGridSize);
-    console.log("howManyGridItemsWide: " + howManyGridItemsWide);
-    for (let i = 0; i < howManyGridItemsWide; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        // gridItem.style.width = `${widthOfGridItems}px`;
-        // gridItem.style.height = `${widthOfGridItems}px`;
-        gameGrid.appendChild(gridItem);
-        // gameGrid.append(gridItem)
-        console.log("appended grid item!");
+    for (let i = 0; i < gameGridSize; i++) {
+        for (let j = 0; j < gameGridSize; j++) {
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gameGrid.appendChild(gridItem);
+        }
     }
-    // gameGrid.appendChild(gridItem);
+
+    // // const gridItem = document.createElement('div');
+    // // gridItem.classList.add('grid-item');
+
+    // console.log(gameGrid.offsetWidth);
+    // console.log(gameGrid.offsetHeight);
+
+    // const howManyGridItemsWide = Math.floor(gameGrid.offsetWidth / gameGridSize);
+    // console.log("howManyGridItemsWide: " + howManyGridItemsWide);
+    // for (let i = 0; i < howManyGridItemsWide; i++) {
+    //     const gridItem = document.createElement('div');
+    //     gridItem.classList.add('grid-item');
+    //     // gridItem.style.width = `${widthOfGridItems}px`;
+    //     // gridItem.style.height = `${widthOfGridItems}px`;
+    //     gameGrid.appendChild(gridItem);
+    //     // gameGrid.append(gridItem)
+    //     console.log("appended grid item!");
+    // }
+    // // gameGrid.appendChild(gridItem);
 
 });
