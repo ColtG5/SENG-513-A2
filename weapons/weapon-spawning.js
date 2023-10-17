0870962b3596036b84be04d1579b016aba96d6ac
Colton Gowans <coltongowans@gmail.com>
2023-10-17 11:51:55 -0600

    rapid firing complete

--- weapons/weapon-spawning.js
+++ weapons/weapon-spawning.js
@@ -1,45 +1,72 @@
-import { createBullet } from '../weapons/weapons.js';
-import { onWindow } from '../utility.js';
-import { gunImage } from '../character/character.js';
-import { getDirectionToMove } from '../game/update-objects.js';
+import { createBullet } from "../weapons/weapons.js";
+import { onWindow } from "../utility.js";
+import { gunImage } from "../character/character.js";
+import { getDirectionToMove } from "../game/update-objects.js";
 
-const gameContainer = document.getElementById('game-container');
-const character = document.getElementById('character');
+const gameContainer = document.getElementById("game-container");
+const character = document.getElementById("character");
 
-let bullets = []
+let bullets = [];
 const bulletWidth = 10;
 const bulletHeight = 10;
-const rapidFireDelay = 100;
+const rapidFireDelay = 150;
 
 const seperationFromCharacter = 30;
 
 let mouseDown = false;
-gameContainer.addEventListener('mousedown', (event) => {
+let mouseX, mouseY;
+
+let rapidFiring = false;
+gameContainer.addEventListener("mousedown", (event) => {
     if (onWindow === true) {
         mouseDown = true;
-        // while (mouseDown) {
-        //     print("whilellll")
-        //     setTimeout(() => {
-        //         print("spawning bullet")
-        //         spawnBullet(event);
-        //     }, rapidFireDelay);
-        // }
+
+        mouseX = event.clientX - gameContainer.offsetLeft;
+        mouseY = event.clientY - gameContainer.offsetTop;
+
+        // this below code is hilarious
+
+        // const rapidFireDelayInterval = setInterval(() => {
+        //     spawnBullet(event);
+        //     if (mouseDown) {
+        //         rapidFireDelayInterval.clearInterval();
+        //     }
+        // }, rapidFireDelay);
+
+        if (rapidFiring === false) {
+            rapidFiring = true;
+            const rapidFireDelayInterval = setInterval(() => {
+                spawnBullet(event);
+                if (!mouseDown) {
+                    rapidFiring = false;
+                    clearInterval(rapidFireDelayInterval);
+                }
+            }, rapidFireDelay);
+        }
+
+
         // spawn a bullet every rapidFireDelay seconds, and stop spawning bullets once mouseDown isnt true
-
     }
-
 });
 
-gameContainer.addEventListener('mouseup', (event) => {
+gameContainer.addEventListener("mousemove", (event) => {
+    if (onWindow === true && mouseDown) {
+        // Update the mouse position as the mouse moves
+        mouseX = event.clientX - gameContainer.offsetLeft;
+        mouseY = event.clientY - gameContainer.offsetTop;
+    }
+});
+
+gameContainer.addEventListener("mouseup", (event) => {
     if (onWindow === true) {
         mouseDown = false;
     }
 });
 
-function bulletDirection(event) {
+function bulletDirection() {
     // Calculate the direction vector from character to mouse pointer
-    const mouseX = event.clientX - gameContainer.offsetLeft;
-    const mouseY = event.clientY - gameContainer.offsetTop;
+    // const mouseX = event.clientX - gameContainer.offsetLeft;
+    // const mouseY = event.clientY - gameContainer.offsetTop;
     const characterCenterX = character.offsetLeft + character.offsetWidth / 2;
     const characterCenterY = character.offsetTop + character.offsetHeight / 2;
 
@@ -58,7 +85,7 @@ function bulletDirection(event) {
 
     return {
         nDeltaX: normalizedDeltaX,
-        nDeltaY: normalizedDeltaY
+        nDeltaY: normalizedDeltaY,
     };
 }
 
@@ -66,6 +93,8 @@ function spawnBullet(event) {
     let bullet = createBullet();
     const dx = bulletDirection(event).nDeltaX;
     const dy = bulletDirection(event).nDeltaY;
+
+    gunImage()
 
     // const { dx, dy } = bulletDirection(event)
     // const { dx, dy } = bulletDirection(event);
@@ -79,19 +108,27 @@ function spawnBullet(event) {
 
     // console.log(dx, dy);
 
-    const bulletStartX = (character.offsetLeft + (character.offsetWidth / 2) + dx * seperationFromCharacter) - bulletWidth / 2;
-    const bulletStartY = (character.offsetTop + (character.offsetHeight / 2) + dy * seperationFromCharacter) - bulletHeight / 2;
+    const bulletStartX =
+        character.offsetLeft +
+        character.offsetWidth / 2 +
+        dx * seperationFromCharacter -
+        bulletWidth / 2;
+    const bulletStartY =
+        character.offsetTop +
+        character.offsetHeight / 2 +
+        dy * seperationFromCharacter -
+        bulletHeight / 2;
 
-    bullet.element.style.offsetWidth = bulletWidth + 'px';
-    bullet.element.style.offsetHeight = bulletHeight + 'px';
-    bullet.element.style.left = bulletStartX + 'px';
-    bullet.element.style.top = bulletStartY + 'px';
+    bullet.element.style.offsetWidth = bulletWidth + "px";
+    bullet.element.style.offsetHeight = bulletHeight + "px";
+    bullet.element.style.left = bulletStartX + "px";
+    bullet.element.style.top = bulletStartY + "px";
 
     gameContainer.appendChild(bullet.element);
     // console.log(bullet)
     bullets.push(bullet);
-    console.log('shoot fr fr');
+    console.log("shoot fr fr");
     // console.log(bullets);
 }
 
-export { bullets }
+export { bullets };
